e89161c2ff49c9ffd8ee604bf2b80db1d90773ff
ethanrubio <ethanrubio@gmail.com>
2017-05-24 18:40:41 -0700

    Fix firstPush check

--- ci-services/circleci.js
+++ ci-services/circleci.js
@@ -4,10 +4,19 @@ const gitHelpers = require('../lib/git-helpers')
 
 const env = process.env
 
+function isFirstPush() {
+  if (_.isEmpty(env.firstPush)) {
+    const firstPush = gitHelpers.getNumberOfCommitsOnBranch(env.CIRCLE_BRANCH) === 1;
+    env.firstPush = firstPush;
+    return firstPush;
+  }
+  return env.firstPush;
+}
+
 module.exports = {
   repoSlug: `${env.CIRCLE_PROJECT_USERNAME}/${env.CIRCLE_PROJECT_REPONAME}`,
   branchName: env.CIRCLE_BRANCH,
-  firstPush: gitHelpers.getNumberOfCommitsOnBranch(env.CIRCLE_BRANCH) === 1,
+  firstPush: isFirstPush(),
   correctBuild: _.isEmpty(env.CI_PULL_REQUEST),
   uploadBuild: env.CIRCLE_NODE_INDEX === '0'
 }
