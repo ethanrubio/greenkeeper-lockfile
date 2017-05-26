cacac4defdff9f52ef387a97a86528ad80c4aa40
ethanrubio <ethanrubio@gmail.com>
2017-05-26 09:50:04 -0700

    Update README

--- ci-services/circleci.js
+++ ci-services/circleci.js
@@ -6,9 +6,9 @@ const config = require('../lib/config')
 
 const env = process.env
 
-function isFirstPush (branch) {
+function isFirstPush (branch, sha1) {
   const commitNumber = gitHelpers.getNumberOfCommitsOnBranch(branch)
-  const commitMessage = gitHelpers.getCommitMessage().trim()
+  const commitMessage = gitHelpers.getCommitMessage(sha1).trim()
   // CircleCI enviroment treats first push as 0, and also ignore the commit from greenkeeper update
   const areFirstGreenKeeperCommits = commitNumber === 0 || commitNumber === 1
 
@@ -25,7 +25,7 @@ function isFirstPush (branch) {
 module.exports = {
   repoSlug: `${env.CIRCLE_PROJECT_USERNAME}/${env.CIRCLE_PROJECT_REPONAME}`,
   branchName: env.CIRCLE_BRANCH,
-  firstPush: isFirstPush(env.CIRCLE_BRANCH),
+  firstPush: isFirstPush(env.CIRCLE_BRANCH, env.CIRCLE_SHA1),
   correctBuild: _.isEmpty(env.CI_PULL_REQUEST),
   uploadBuild: env.CIRCLE_NODE_INDEX === '0'
 }
